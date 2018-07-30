e3f47569cc4b3a5011b2f5b42ef291adf1407fd0
YVDV95 <yorickvandervis@ziggo.nl>
2018-07-30 10:09:13 +0200

    Geocoding address changing for API limit

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,14 @@
 class Spot < ApplicationRecord
 
+	geocoded_by :address
+	after_validation :geocode, if: :address_changed?
+
 	def address
   		[street, number, city, state, country].compact.join(', ')
 	end
 
-	geocoded_by :address
-	after_validation :geocode
+	def address_changed?
+		street_changed? || number_changed? || city_changed? || state_changed? || country_changed?
+	end
 	
 end
